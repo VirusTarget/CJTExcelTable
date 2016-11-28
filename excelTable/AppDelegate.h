f1ae90e3c99d3791352f01ec4c641ea634644885
陈晋添 <408069830@qq.com>
2016-11-28 22:04:43 +0800

    Initial Commit

---
+++ excelTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  excelTable
+//
+//  Created by 陈晋添 on 2016/11/28.
+//  Copyright © 2016年 陈晋添. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
